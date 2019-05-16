f3e5c302273d7034db67062405b627b8b5b309ca
Ignat <ignatkalvados@gmail.com>
2019-05-16 11:54:28 +0300

    1405HW

---
+++ javascript/1405/main.js
@@ -0,0 +1,25 @@
+let time = 5;
+    if (time >= 6 && time < 10) {
+    document.write("Сейчас утро<br>");
+    }
+    else if (time >= 10 && time < 15) {
+    document.write("Сейчас обед<br>");
+    }
+    else if (time >= 15 && time < 20) { 
+    document.write("Сейчас вечер<br>");
+    }
+    else if (time >= 20 && time < 22) {
+    document.write("Сейчас закат<br>");
+    }
+    else {
+    document.write("Сейчас ночь<br>");
+    }
+  
+   console.log(typeof(time));
+
+  
+
+  let some_array = ["ProCode", 4, true,];
+  document.write(some_array);
+  console.log(typeof(some_array[0]));
+    

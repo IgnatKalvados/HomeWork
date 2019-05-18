5adcd33c4c9797eaea4e9678638be5819f2df599
Ignat <ignatkalvados@gmail.com>
2019-05-18 15:11:16 +0300

    1705HW

--- javascript/1705/main.js
+++ javascript/1705/main.js
@@ -1,15 +1,16 @@
 let n = +prompt("Цикл с for");
-for (f=1; n; f*=n--); 
+let f = 1;
+for (f=1; n; f*=n--) {
 console.log(f);
-
+}
 
 function factorial(n){
-  let res = 1;
-  
-  while (n--) 
-      res *= n + 1
-  
+  let res = 1;  
+  while (n--) {
+      res *= n + 1  
   return res;
+  }
 }
+
 let num =+prompt("Цикл с while");
 console.log(factorial(num));

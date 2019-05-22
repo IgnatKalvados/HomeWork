60389cb0455a373b9aa3496a660d327cbe907eea
Ignat <ignatkalvados@gmail.com>
2019-05-22 11:09:01 +0300

    2105HW

---
+++ javascript/2105/main.js
@@ -0,0 +1,24 @@
+let exemple_numbers = null;
+let exemple_string = null;
+
+let some_array = [
+    "Пример строки",
+    7,
+    [9, "вложенный массив", 18],
+    "Еще один пример",
+    24,
+    12.7,
+    "28"
+];
+
+for (i=0; i<some_array.length; i++) {
+
+        if (typeof(some_array[i]) === String) {
+            exemple_string.push(some_array[i]);
+        } 
+        else if (typeof(some_array[i]) === Number) {
+            exemple_numbers.push(some_array[i]);
+        }
+}
+console.log(exemple_string);
+console.log(exemple_numbers);

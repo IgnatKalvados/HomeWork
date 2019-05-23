0d1d288f3cd8daa8fcac86949737578edd4a4431
Ignat <ignatkalvados@gmail.com>
2019-05-23 10:44:43 +0300

    2105HW

--- javascript/2105/main.js
+++ javascript/2105/main.js
@@ -6,26 +6,28 @@ let answer = [
 let arr = [
     "Пример строки",
     7,
-    " Еще один пример",
+    [" Еще один пример", "пример вложенного массива"],
     24,
     12.7,
     34,
     47,
     " hello"
 ];
-
-for (i=0; i<arr.length; i++) {    
-        if (typeof(arr[i]) === "string") {
-        answer[0].push(arr[i]);
-        } 
-       else if (typeof(arr[i]) === "number") {
-            answer[1].push(arr[i]);
-        }
-        
+for (k=0; k<arr.length; k++) {
+    // let i = arr[k];
+    // for (i=0; i<k.length; i++) {    
+    //         if (typeof(i) === "string") {
+    //         answer[0].push(i);
+    //         } 
+    //     else if (typeof(i) === "number") {
+    //             answer[1].push(i);
+    //         }
+            
+    // } 
+    console.log(typeof(k.length-1));
 }
-
-document.write(str);
-document.write("</br>" + num);
+// document.write(str);
+// document.write("</br>" + num);
 
 
 

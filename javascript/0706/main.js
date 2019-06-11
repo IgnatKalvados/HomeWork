774f76e87f2b845399df434ebba4f3f8328d9bb3
Ignat <ignatkalvados@gmail.com>
2019-06-11 09:58:45 +0300

    0706HW

---
+++ javascript/0706/main.js
@@ -0,0 +1,33 @@
+let ball = document.querySelector('.ball');
+
+ball.addEventListener('mousedown', mouseDown);
+ball.addEventListener('mouseup', mouseUp);
+
+function mouseDown(e){
+    
+    ball.style.position = 'absolute';
+    move(e);  
+    ball.style.zIndex = 1000;
+    
+    function move(e) {
+        ball.style.left = e.pageX - ball.offsetWidth / 2 + 'px';
+        ball.style.top = e.pageY - ball.offsetHeight / 2 + 'px';      
+    }
+    
+    ball.addEventListener('mousemove',function(e){
+        move(e); 
+    });    
+} 
+
+function mouseUp(){
+    console.log('end');   
+    ball.removeEventListener('mousemove');
+    ball.removeEventListener('mousedown', mouseDown);   
+   
+}
+
+
+
+
+
+

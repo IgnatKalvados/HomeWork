f1b333692ae3b99963767780ffa9067a5f6dd2a3
Ignat <ignatkalvados@gmail.com>
2019-07-13 13:57:03 +0300

    server

---
+++ node/server.js
@@ -0,0 +1,22 @@
+const http = require('http');
+
+let server = http.createServer(function(req, res){
+    res.writeHead( 200, {'Content-Type': 'text/html'});
+    res.end(`
+        <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>node server</title>
+</head>
+<body>
+    <h1>Hello World!</h1>
+</body>
+</html>`
+    );
+});
+
+server.listen(3000, '127.0.0.1');
+console.log('Node is working')
